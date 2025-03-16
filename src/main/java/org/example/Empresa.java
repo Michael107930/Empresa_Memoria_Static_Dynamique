7129f9e026fc672d86711ee2df9f933e0af7abaa
Diego <dmsc20052502sanchez@gmail.com>
2025-03-16 17:59:20 -0500

    Descripción de los cambios

--- src/main/java/org/example/Empresa.java
+++ src/main/java/org/example/Empresa.java
@@ -9,21 +9,11 @@ public class Empresa {
     public Empresa(){
         this.empleados= new ArrayList<>();
     }
+    public void agregarEmpleado(String nombre,double salario ){
+        Empleado nuevoEmpleado=new Empleado(nombre,salario);
+        git add .
 
-    public void agregarEmpleado(String nombre, double salario){
-        Empleado nuevoEmpleado = new Empleado(nombre,salario);
-        empleados.add(nuevoEmpleado);
-        System.out.println("El empleado ha sido agregado exitosamente.");
     }
-    public void listarEmpleados(){
-        if (empleados.isEmpty()){
-            System.out.println("No hay empleados registrados");
-            return;
-        } else {
-            for (Empleado empleado: empleados){
-                empleado.mostrarInfo();
-            }
-            System.out.println("Total de empleados enlistados: "+Empleado.getcontadorEmpleados());
-        }
-    }
+
+
 }
